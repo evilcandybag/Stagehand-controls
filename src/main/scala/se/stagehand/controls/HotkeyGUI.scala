43b50c8370f8779980129783a8b16afec9e5d111
Peter <peter@insella.se>
2014-01-09 00:21:39 +0100

    Is now an infix that adds a Label (<key>) to all of its recieving nodes

--- src/main/scala/se/stagehand/controls/HotkeyGUI.scala
+++ src/main/scala/se/stagehand/controls/HotkeyGUI.scala
@@ -11,6 +11,8 @@ import se.stagehand.swing.player.PlayerScriptNode
 import se.stagehand.swing.player.Player
 import scala.swing.event.KeyReleased
 import se.stagehand.lib.Log
+import se.stagehand.swing.player.PlayerGUIElement
+import se.stagehand.swing.player.PlayerScriptInfix
 
 object HotkeyGUI extends ScriptGUI {
   val peer = classOf[Hotkey]
@@ -30,8 +32,9 @@ object HotkeyGUI extends ScriptGUI {
   }
   
   def playerNode(script: ScriptComponent) = {
-    if (script.getClass == peer)
+    if (script.getClass == peer) {
       new HotkeyPlayerNode(script.asInstanceOf[Hotkey])
+    }
     else 
       throw new IllegalArgumentException("script needs to be a hotkey")
   }
@@ -40,13 +43,13 @@ object HotkeyGUI extends ScriptGUI {
 class HotkeyButton(script: Hotkey) extends EditorScriptButton(script) {
 }
 
-class HotkeyPlayerNode(script: Hotkey) extends PlayerScriptNode[Hotkey](script) {
+class HotkeyPlayerNode(script: Hotkey) extends PlayerScriptInfix[Hotkey](script) {
   private val log = Log.getLog(this.getClass())
   def title = script.displayName
   
   KeyEventHandler.register(this)
   
-  layout(new Label(script.key.toString)) = BorderPanel.Position.Center
+  def infix = new Label("(" + script.key.toString + ")" )
   reactions += {
     case e:KeyReleased if e.key == script.key => {
       log.debug("Pressed " + e.key + " in script " + script.key )
